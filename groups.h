d1ff95a24d5430f4562ccfabbb0c08846faedb1e
LiubomyrFadieiev <61575710+LiubomyrFadieiev@users.noreply.github.com>
2020-04-01 16:15:59 +0300

    Add files via upload

---
+++ groups.h
@@ -0,0 +1,7 @@
+#pragma once
+#include <string>
+using namespace std;
+void read_groups(string arr[], int rows);
+int count_groups(string arr[]);
+void change_groups(string arr[], int rows);
+void write_groups(string arr[], int count);
