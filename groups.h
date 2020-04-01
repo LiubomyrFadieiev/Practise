a1d20faaf71d1805519bf577faf6068ecd212de0
LiubomyrFadieiev <61575710+LiubomyrFadieiev@users.noreply.github.com>
2020-04-01 16:16:28 +0300

    Delete groups.h

--- groups.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include <string>
-using namespace std;
-void read_groups(string arr[], int rows);
-int count_groups(string arr[]);
-void change_groups(string arr[], int rows);
-void write_groups(string arr[], int count);
